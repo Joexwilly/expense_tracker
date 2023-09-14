628fce14e19bdd71ef6ec4b9764ed1b8058c7d6d
Joexwilly <joeguyski@gmail.com>
2023-09-14 17:57:43 +0100

    added validation for saving new expense

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -28,6 +28,32 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount =
+        double.tryParse(_amountController.text); //tryparse('hello') =>null
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text('Please enter valid title and amount'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+  }
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -118,10 +144,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Add Expense'),
               ),
             ],
