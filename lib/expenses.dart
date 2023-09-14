51999b5d87b85748f3cdbe07a01efd5732e99880
Joexwilly <joeguyski@gmail.com>
2023-09-14 19:49:54 +0100

    worked on expense removal

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -49,13 +49,40 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense); //remove expense
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Expense deleted'),
+        duration: const Duration(seconds: 3),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expesnses found. Start adding some?'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Expense Tracker'),
@@ -67,11 +94,7 @@ class _ExpensesState extends State<Expenses> {
         body: Column(
           children: [
             Text('The chart'),
-            Expanded(
-              child: ExpensesList(
-                  expenses: _registeredExpenses,
-                  onRemoveExpense: _removeExpense),
-            ),
+            Expanded(child: mainContent),
           ],
         ));
   }
